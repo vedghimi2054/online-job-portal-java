9d270489dbfe4c7ae42dbfa0ccbe9c7082802bb4
sushilghimire2054 <ghimires2054@gmail.com>
2022-05-24 20:48:48 +0545

    modified:native query with dao

--- src/main/java/com/youtube/jwt/dao/PostRepository.java
+++ src/main/java/com/youtube/jwt/dao/PostRepository.java
@@ -4,12 +4,11 @@ import com.youtube.jwt.entity.JobPosts;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface PostRepository extends CrudRepository<JobPosts,Integer> {
 
-    @Query(value = "select from job_posts where concat(company_name,description,job_title,last_date,post_date,user_name) like %?1%",nativeQuery = true)
+    @Query(value = "select * from job_posts where concat(company_name,description,job_title,last_date,post_date,user_name) like %?1%",nativeQuery = true)
     List<JobPosts> searchJobPostsBy(String keyword);
 }
